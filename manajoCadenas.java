74b2bb8bd911dabc335940bf045c4ee235c7c751
mariluzataucuri <49160449+mariluzataucuri@users.noreply.github.com>
2019-04-01 10:08:03 -0500

    Add files via upload

---
+++ manajoCadenas.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author PC03-LAB02
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Mary";
+        String apellido = "Ataucuri";
+        System.out.println ("concatinacion:"+nombre+apellido);
+        System.out.println("nueva linea:\n"+nombre+" "+ apellido);
+        System.out.println("tabulador:\t"+nombre+" "+apellido);
+        System.out.println("retroceso:\b"+nombre+" "+apellido);
+        System.out.println("retorno de carro:\r"+nombre+" "+apellido);
+        System.out.println("comilla simple:\'"+nombre+" "+apellido);
+        System.out.println("comilla doble:\""+nombre+" "+apellido+
+        
+    }
+    
+}
