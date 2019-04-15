675f2df47e99c4fa07f6e8c5e5ed96e569d03cfd
mariluzataucuri <49160449+mariluzataucuri@users.noreply.github.com>
2019-04-15 10:17:59 -0500

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica02;
+
+/**
+ *
+ * @author PC15-LAB02
+ */
+public class Practica02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //variable byte
+        byte b1=10;
+        byte b2=0xa;
+        //variable short
+        short s1=2;
+        //variable char
+        char ch1=65,ch2='A';
+        //variable enteros
+        int decimal=100;
+        int octal=0144;
+        int hexa=0*64;
+        //variable long
+        long long1=10,long2=20l;
+        //variable float
+        float f1=15,f2=22.3f;
+        //variable double
+        double d1=11.0,d2=30.15d;
+    }
+    
+}
