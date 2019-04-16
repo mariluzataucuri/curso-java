9284a8e1a696b18ea331755c8d9fd09a28319203
mariluzataucuri <49160449+mariluzataucuri@users.noreply.github.com>
2019-04-16 12:22:33 -0500

    Add files via upload

---
+++ while.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PC03-LAB02
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n=500;
+        int c=1;
+        while (c<=n){
+            System.out.println(c);
+            c=c+1;
+        }
+                
+    }
+    
+}
