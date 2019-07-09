8e3bbdd8bf60cada3c31f8cf9fd69ad860f0e62b
mariluzataucuri <49160449+mariluzataucuri@users.noreply.github.com>
2019-07-09 11:41:52 -0500

    Add files via upload

---
+++ SumaDeLosNumeros.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma.de.los.numeros;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PC03-LAB02
+ */
+public class SumaDeLosNumeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[]l=new int[n];
+        int i=0;
+        while (i<l .length){
+        System.out .println("INGRESA NUM:");
+         l[i]=scanner.nextInt();
+         i++;
+         
+    }
+        
+    }
+    
+}
